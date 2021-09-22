94d6d00fa70efdcafbffb12b74842bac2dfbed2d
Adonay JP <adonayjp.5@gmail.com>
2021-09-22 00:22:22 -0500

    se crea el primer modulo, serv y componente en la app

---
+++ src/app/components/usuarios/cat-usuarios/cat-usuarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-usuarios',
+  templateUrl: './cat-usuarios.component.html',
+  styleUrls: ['./cat-usuarios.component.sass']
+})
+export class CatUsuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
